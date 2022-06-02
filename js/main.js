5502e92f04124bb78b774c5f41b0cf52d3cc300f
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2022-06-02 18:47:23 +0500

    mobile app page updated

--- js/main.js
+++ js/main.js
@@ -686,3 +686,31 @@ $(document).on('click', '#headingtab a', function(e) {
 		nav.find('a').tab('show');
 	}
 });
+
+// Isotppe Filter
+$(".filter-controls li").on('click', function() {
+	var filterData = $(this).attr("data-filter");
+
+	$(".portfolio-filter, .gallery-filter").isotope({
+		filter: filterData,
+	});
+
+	$(".filter-controls li").removeClass('active');
+	$(this).addClass('active');
+});
+
+$(".portfolio-filter, .gallery-filter").isotope({
+	itemSelector: '.pf-item, .gf-item',
+	percentPosition: true,
+	masonry: {
+	// use element for option
+	columnWidth: '.pf-item, .gf-item',
+	horizontalOrder: true,
+  }
+});
+
+//Masonary
+$('.portfolio-details-pic').masonry({
+	itemSelector: '.pdp-item',
+	columnWidth: '.pdp-item'
+});
