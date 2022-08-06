8168f79cb561342f45f686ca2dd2e83255a14dbf
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2022-08-05 21:29:58 -0700

    vr page created

--- js/main.js
+++ js/main.js
@@ -767,6 +767,43 @@ File Description: Main JS file of the template*/
 	  	}
   	});
 
+	//Owl-Carousel - vr-business-slider
+	$('.vr-business-slider').owlCarousel({
+		items: 4,
+		loop: true,
+		autoplay: true,
+		centre: true,
+		margin: 10,
+		nav: true,
+		dots: false,
+		navText : ["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		smartSpeed: 2000,
+		responsiveClass: true,
+		rewind: true,
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  520: {
+			items: 1
+		  },
+		  768: {
+			items: 3
+		  },
+		  1200: {
+			items: 3
+		  },
+		  1400: {
+			items: 3
+		  },
+		  1600: {
+			items: 3
+		  },
+		}
+	});
+
 	//Counter Up	
 	$(".counter").counterUp({
 		delay: 10,
